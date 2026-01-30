3c733866e1d3e6996e2af111bf636f5cd2a69497
ParkJunYong <dyden119@gmail.com>
2026-01-30 10:58:35 +0900

    자바는 처음에 할게 뭐이리 많냐

---
+++ backend/src/test/java/com/example/order_inventory/OrderInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.order_inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
